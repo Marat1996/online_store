ec2f4dfa69c22b216cf0a47e2b324d3b311a8699
Marat Zaripov <maratos96@gmail.com>
2024-03-20 20:41:11 +0300

    Правки

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name='home'),
-    path('contact/', views.contact_view, name='contact'),
+    path('', views.home, name='home'),
+    path('contacts/', views.contacts, name='contacts'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-def home_view(request):
+def home(request):
     return render(request, 'catalog/home.html')
 
-def contact_view(request):
+def contacts(request):
     return render(request, 'catalog/contact.html')
