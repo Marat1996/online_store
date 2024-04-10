553d4db39aaf5172c1ee4e90312af2849708533a
Marat Zaripov <maratos96@gmail.com>
2024-04-10 21:11:35 +0300

    admin

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,9 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.product_detail, name='product_detail'),
     path('contacts/', views.contacts, name='contacts'),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
