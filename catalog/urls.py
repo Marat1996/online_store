85022726250a67e7e9840d54e2ef9baa2aaabc36
Marat Zaripov <maratos96@gmail.com>
2024-04-21 18:38:40 +0300

    правки

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('category/<slug:slug>/', views.category_products, name='category_products'),
     path('contacts/', views.contacts, name='contacts'),
     path('cart/', views.cart, name='cart'),
-    path('add-to-cart/', views.add_to_cart, name='add_to_cart'),
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
 ]
